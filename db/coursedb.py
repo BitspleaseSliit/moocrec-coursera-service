b4ca2f454f22a70c411d563274d08d59a0cd4e46
sajeeva <sa.hasith@gmail.com>
2018-09-11 14:18:42 +0530

    updateVideoStyle method in coursesdb

--- db/coursedb.py
+++ db/coursedb.py
@@ -1,4 +1,5 @@
 import pymongo
+from bson.objectid import ObjectId
 from dbConnection import ( db )
 
 courses = db["test_courses"]
@@ -6,7 +7,7 @@ courses = db["test_courses"]
 def getAll():
     courses_array = []
     for course in courses.find():
-        # print(course["name"])
+        # print(course["_id"])
         courses_array.append(course)
 
     return courses_array
@@ -15,8 +16,25 @@ def getDownlodCourses():
     download_courses_array = []
     for course in courses.find({"processed": False, "download": True}):
         download_courses_array.append(course)
-    print(download_courses_array)
+    # print(download_courses_array)
 
     return download_courses_array
 
-# def updateVideoStyle(video_style, course):
+def updateVideoStyle(video_style, course):
+    select_query = { "_id": ObjectId(course["_id"])}
+    insert_value = { "$set": { "videoStyle": video_style} }
+    courses.update_one(select_query, insert_value)
+
+    return True
+
+# mycourse = getAll()[0]
+# print(mycourse)
+# videoStyle = {}
+# videoStyle["talkingHead"] = 30
+# videoStyle["slide"] = 10
+# videoStyle["code"] = 60
+# videoStyle["conversation"] = 0
+# videoStyle["animation"] = 0
+# videoStyle["whiteboard"] = 0
+# print(videoStyle)
+# updateVideoStyle(videoStyle, mycourse)
