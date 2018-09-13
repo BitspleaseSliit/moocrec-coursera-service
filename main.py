52c9b264a8b02da069661d9e51b4a56f79a8d4bf
sajeeva <sa.hasith@gmail.com>
2018-09-13 14:24:34 +0530

    delete after finish the prosess

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+import os, stat
+import shutil
 from scrapper.coursera_dl import ( scrapper )
 from db.coursedb import ( Courses )
 
 
+def deleteDownloadContent(course):
+    print("deleting files ....... ")
+
+    jsonFile = course["path"] + "-syllabus-parsed.json"
+
+    if os.path.exists(jsonFile):
+        os.remove(jsonFile)
+    else:
+        print("The file does not exist")
+        return False
+
+    if os.path.exists(course["path"]):
+        shutil.rmtree(course["path"])
+    else:
+        print("The file does not exist")
+        return False
+
 
 if __name__ == '__main__':
-    # scrapper()
+    
     courses = Courses()
-    # print(courses.getDownloadCoursesPaths())
-
+    print(courses.getDownloadCoursesPaths())
     download_courses = courses.getDownlodCourses()
 
     for course in download_courses:
         if scrapper(course["path"]):
             courses.updateProcessedTrue(course)
-            print("course download fail")
+            print("course download successfull")
+            deleteDownloadContent(download_courses[0])
         else:
             courses.updateProcessedFalse(course)
-            print("course download successfull")
+            print("course download Fail")
         
